c5fa57b656dd1ee537702528b04dc7a5d1bca976
Jaspal <jaspal.iiith@gmail.com>
2014-02-19 16:37:38 +0530

    malloc(),free() made thread_safe. realloc remains.

--- malloc.c
+++ malloc.c
@@ -1,8 +1,11 @@
 /* A simple allocation of the malloc routine */ 
 #include<unistd.h> 
 #include<stdio.h>
+#include<pthread.h>
 #define align4(x)	(((((x)-1)>>2)<<2)+4)
 void *base=NULL;
+
+pthread_mutex_t thread_lock;
 
 typedef struct s_block* t_block;
 struct s_block
@@ -54,18 +57,23 @@ void* malloc(size_t size)
 	//printf("yupieee, malloc got called finally, size_requested = %zu\n",size);
 	size_t s = align4(size);
 	t_block b,last;
+	pthread_mutex_lock(&thread_lock);
 	if(base==NULL){
 		b = extend_heap(NULL,s);
-		if(!b)
+		if(!b){
+			pthread_mutex_unlock(&thread_lock);
 			return NULL;
+		}
 		base=b;
 	}
 	else{
 		b=find_block(&last,s);
 		if(!b){
 			b = extend_heap(last,s);
-			if(!b)
+			if(!b){
+				pthread_mutex_unlock(&thread_lock);
 				return NULL;
+			}
 		}
 		else{
 			if(b->size-s>= sizeof(struct s_block)+4)
@@ -73,6 +81,7 @@ void* malloc(size_t size)
 			b->free=0;
 		}
 	}
+	pthread_mutex_unlock(&thread_lock);
 	return (void*)((char*)b+BLOCK_SIZE);
 }
 
@@ -106,15 +115,19 @@ void coalesce(t_block b)
 
 void free(void *p)
 {
+	pthread_mutex_lock(&thread_lock);
 	t_block last;
 	t_block b = valid_addr(p,&last);
-	if(!b)
+	if(!b){
+		pthread_mutex_unlock(&thread_lock);
 		return;
+	}
 	b->free = 1;
 	if(b->next && b->next->free==1)
 		coalesce(b);
 	if(last && last->free==1)
 		coalesce(last);
+	pthread_mutex_unlock(&thread_lock);
 	return;
 }
 
